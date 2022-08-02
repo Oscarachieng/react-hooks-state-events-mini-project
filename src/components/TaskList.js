29357cc0c755b48501d1f74592f084acb3eef538
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-02 17:01:42 +0300

    commit tree 284368a245a388332c2dc7bc2a188bb6c9043156

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks, handleTaskDeleteClick }) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map(task => <Task task={task} key={task.text} handleTaskDeleteClick={handleTaskDeleteClick} />)}
     </div>
   );
 }
